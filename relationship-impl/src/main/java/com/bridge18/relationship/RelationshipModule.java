105d46d169c58462424d97ed7def42d456583b55
Viktor Zikratyi <victor.zikratyi@bridge18.com>
2017-03-29 16:22:36 +0300

    initial commit

---
+++ relationship-impl/src/main/java/com/bridge18/relationship/RelationshipModule.java
@@ -0,0 +1,9 @@
+package com.bridge18.relationship;
+
+import com.google.inject.AbstractModule;
+import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+
+public class RelationshipModule extends AbstractModule implements ServiceGuiceSupport {
+    @Override
+    protected void configure() {}
+}
